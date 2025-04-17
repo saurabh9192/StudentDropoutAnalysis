de10538eb742df980051f0c16ab48081ea5effbb
Saurabh Deshmukh <122543163+Saurabh911264@users.noreply.github.com>
2025-04-17 23:22:48 +0530

    Changes in StudentTable.jsx

--- frontend/src/pages/StudentTable.jsx
+++ frontend/src/pages/StudentTable.jsx
@@ -1,7 +1,6 @@
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
 export default function StudentTable() {
   const [users, setUsers] = useState([]);
   useEffect(() => {
