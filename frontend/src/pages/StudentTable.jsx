ba25ae6996b27a962bca492a737925f60bda760b
Saurabh Deshmukh <122543163+Saurabh911264@users.noreply.github.com>
2025-04-18 08:12:22 +0530

    Changes(Unused Axios) in StudentTable.jsx

--- frontend/src/pages/StudentTable.jsx
+++ frontend/src/pages/StudentTable.jsx
@@ -1,6 +1,5 @@
 
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 export default function StudentTable() {
   const [students, setStudents] = useState([]);
   useEffect(() => {
